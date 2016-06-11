43a91ce86f8a2083e3b121c5351c7f7cd250e302
Lxxyx <841380530@qq.com>
2016-06-11 10:19:41 +0800

    change count length

--- countLength.js
+++ countLength.js
@@ -1,45 +1,42 @@
 /**
  * 计算所写博客字数。Node直接运行即可。
  */
-var fs = require('fs');
+var fs = require('fs')
 
-var totalLength = 0;
-var commonPath = './source/_posts/';
+var totalLength = 0
+var commonPath = './source/_posts/'
 
-var getDirFilesArray = function(path) {
-  return new Promise(function(reslove, reject) {
-    fs.readdir(path, (err, data) => {
-      if (data) {
-        reslove(data);
-      } else {
-        reject(err)
-      }
-    });
-  });
-};
+var getDirFilesArray = path => new Promise((reslove, reject) => {
+  fs.readdir(path, (err, data) => {
+    if (data) {
+      reslove(data)
+    } else {
+      reject(err)
+    }
+  })
+})
 
-var getFileLength = function(fileName) {
-  return new Promise(function(reslove, reject) {
-    fs.readFile(commonPath + fileName, 'utf-8', (err, data) => {
-      if (data) {
-        reslove(data)
-      } else {
-        reject(err)
-      }
-    });
-  });
-};
+var getFileLength = fileName => new Promise((reslove, reject) => {
+  fs.readFile(commonPath + fileName, 'utf-8', (err, data) => {
+    if (data) {
+      reslove(data)
+    } else {
+      reject(err)
+    }
+  })
+})
+
 
 getDirFilesArray(commonPath)
-  .then((response) => {
-    for (x in response) {
-      getFileLength(response[x])
+  .then(res => {
+    for (x in res) {
+      getFileLength(res[x])
         .then((fileData) => {
-          totalLength += fileData.length;
-          console.log(totalLength);
+          totalLength += fileData.length
+          console.log(totalLength)
         })
     }
   })
-  .catch((error) => {
-    console.log(error)
-  });
+  .catch(e => {
+    console.log(e)
+  })
