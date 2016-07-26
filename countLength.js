2119871178e9ebdc355ca299ed2cbcd9e942b4c6
刘子健 <841380530@qq.com>
2016-07-26 15:50:51 +0800

    refactor(计算字数): 加入文件夹遍历

--- countLength.js
+++ countLength.js
@@ -7,6 +7,18 @@ let totalLength = 0
 const commonPath = './source/_posts/'
 
 let dir = fs.readdirSync(commonPath)
-dir.forEach(val => totalLength += fs.readFileSync(`${commonPath}${val}`, 'utf-8').length)
+
+function walkDir (dir, dirPath) {
+  dir.forEach(val => {
+    const stat = fs.statSync(`${dirPath}${val}`)
+    if (stat.isDirectory()) {
+      walkDir(fs.readdirSync(`${dirPath}${val}`), `${dirPath}${val}/`)
+    } else {
+      totalLength += fs.readFileSync(`${dirPath}${val}`, 'utf-8').length
+    }
+  })
+}
+
+walkDir(dir, commonPath)
 
 console.log(`博客总字数为：${totalLength}`)
