d83e648e57278712feb2935e441d46fb08122a5f
Lxxyx <Lxxyxzj@gmail.com>
2018-04-18 08:02:44 +0800

    feat(生成): 重构生成

--- new_blog.js
+++ new_blog.js
@@ -1,25 +1,30 @@
-const fs = require('fs');
-const childProcess = require('child_process');
-const path = require('path');
+const fs = require('fs')
+const childProcess = require('child_process')
+const path = require('path')
 
-const filename = process.argv[2];
-const posts = path.resolve(__dirname, './source/_posts/');
-const targetDir = path.resolve(__dirname, './source/_posts/2018/');
+const currentYear = new Date().getFullYear()
+const filename = process.argv[2]
+const posts = path.resolve(__dirname, './source/_posts/')
+const targetDir = path.resolve(__dirname, './source/_posts/', currentYear)
 
-if (!filename) {
-  throw new Error('需要传入博客名');
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir)
 }
 
-childProcess.execSync(`hexo n '${filename}'`);
+if (!filename) {
+  throw new Error('需要传入博客名')
+}
 
-const md = path.resolve(posts, `${filename}.md`);
-const targetMD = path.resolve(targetDir, `${filename}.md`);
+childProcess.execSync(`hexo n '${filename}'`)
 
-fs.copyFileSync(md, targetMD);
-fs.unlinkSync(md);
+const md = path.resolve(posts, `${filename}.md`)
+const targetMD = path.resolve(targetDir, `${filename}.md`)
 
-console.log();
-console.log('---------------------');
-console.log(`生成成功`);
-console.log('---------------------');
-console.log();
+fs.copyFileSync(md, targetMD)
+fs.unlinkSync(md)
+
+console.log()
+console.log('---------------------')
+console.log(`生成成功`)
+console.log('---------------------')
+console.log()
