c85e2e5726f7ac19a362c763d1d3968d9ba4855a
fanyi.lzj <fanyi.lzj@alibaba-inc.com>
2018-10-20 22:04:39 +0800

    update

--- new_blog.js
+++ new_blog.js
@@ -4,8 +4,8 @@ const path = require('path')
 
 const currentYear = new Date().getFullYear()
 const filename = process.argv[2]
-const posts = path.resolve(__dirname, './source/_posts/')
-const targetDir = path.resolve(__dirname, './source/_posts/', currentYear)
+const posts = path.resolve(__dirname, './source/_posts')
+const targetDir = path.resolve(posts, `${currentYear}`)
 
 if (!fs.existsSync(targetDir)) {
   fs.mkdirSync(targetDir)
@@ -17,8 +17,9 @@ if (!filename) {
 
 childProcess.execSync(`hexo n '${filename}'`)
 
-const md = path.resolve(posts, `${filename}.md`)
-const targetMD = path.resolve(targetDir, `${filename}.md`)
+const mdName = filename.replace(/_/g, '-') + '.md'
+const md = path.resolve(posts, mdName)
+const targetMD = path.resolve(targetDir, mdName)
 
 fs.copyFileSync(md, targetMD)
 fs.unlinkSync(md)
