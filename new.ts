1e040635f6918f79eeb121d531148df990250f64
fanyi.lzj <fanyi.lzj@alibaba-inc.com>
2020-02-09 13:26:39 +0800

    auto backup

--- new.ts
+++ new.ts
@@ -1,28 +1,25 @@
-const fs = require('fs')
-const childProcess = require('child_process')
-const path = require('path')
+import cp from 'child_process'
+import fs from 'fs-extra'
+import path from 'path'
 
 const currentYear = new Date().getFullYear()
 const filename = process.argv[2]
 const posts = path.resolve(__dirname, './source/_posts')
 const targetDir = path.resolve(posts, `${currentYear}`)
 
-if (!fs.existsSync(targetDir)) {
-  fs.mkdirSync(targetDir)
-}
+fs.ensureDirSync(targetDir)
 
 if (!filename) {
   throw new Error('需要传入博客名')
 }
 
-childProcess.execSync(`hexo n '${filename}'`)
+cp.execSync(`hexo n '${filename}'`)
 
 const mdName = filename.replace(/_/g, '-') + '.md'
 const md = path.resolve(posts, mdName)
 const targetMD = path.resolve(targetDir, mdName)
 
-fs.copyFileSync(md, targetMD)
-fs.unlinkSync(md)
+fs.moveSync(md, targetMD)
 
 console.log()
 console.log('---------------------')
